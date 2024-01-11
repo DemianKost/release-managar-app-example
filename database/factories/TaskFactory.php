cdbb78b6871f5c53a7369f6d43b9c32d1aa0fa6f
DemianWHT <demiankost2@gmail.com>
2024-01-12 00:38:16 +0100

    Update TaskFactory.php

--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -1,7 +1,11 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Database\Factories;
 
+use App\Models\Project;
+use App\Models\Release;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -10,14 +14,17 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 class TaskFactory extends Factory
 {
     /**
-     * Define the model's default state.
-     *
      * @return array<string, mixed>
      */
     public function definition(): array
     {
         return [
-            //
+            'title' => $this->faker->words(nb: 5),
+            'description' => $this->faker->paragraphs(nb: 6, asText: true),
+            'criteria' => $this->faker->boolean() ? $this->faker->paragraphs(nb: 10, asText: true) : null,
+            'complete' => $this->faker->boolean(),
+            'project_id' => Project::factory(),
+            'release_id' => $this->faker->boolean() ? Release::factory() : null,
         ];
     }
 }
