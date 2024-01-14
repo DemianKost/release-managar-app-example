2efb8059e176d7970a393431a50388071f3dd2f5
DemianWHT <demiankost2@gmail.com>
2024-01-14 14:52:03 +0100

    Added form for dashboard page

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,12 +1,24 @@
 <script setup>
-import { computed, defineProps } from 'vue';
-import { usePage } from '@inertiajs/vue3';
+import { computed, defineProps, ref, reactive } from 'vue';
+import { usePage, useForm } from '@inertiajs/vue3';
 import AppLayout from '@/Layouts/AppLayout.vue';
 
 const page = usePage();
 const user = computed(() => {
     return page.props.auth.user;
 });
+
+const open = ref(false);
+
+const form = useForm({
+    title: null,
+    description: null,
+    active: true,
+});
+
+function submit() {
+    form.post( this.$route('app:projects:store') );
+}
 
 defineProps({
     projects: {
@@ -24,6 +36,43 @@ defineProps({
             </h2>
         </template>
 
+        <div v-if="open" class="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+            <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
+            <div class="fixed inset-0 z-10 w-screen overflow-y-auto">
+                <form class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                    <div class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                        <div class="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+                            <div class="sm:flex sm:items-start">
+                                <div class="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                                    <svg class="h-6 w-6 text-red-600" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z" />
+                                    </svg>
+                                </div>
+                                <div class="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                                    <h3 class="text-base font-semibold leading-6 text-gray-900" id="modal-title">Add new project</h3>
+                                    <div class="mt-2">
+                                        <div class="mb-2">
+                                            <label for="title" class="block mb-2 text-sm font-medium text-gray-900">Title</label>
+                                            <input type="text" id="title" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" required>
+                                        </div>
+                                        <div class="mb-2">
+                                            <label for="title" class="block mb-2 text-sm font-medium text-gray-900">Description</label>
+                                            <textarea type="text" id="title" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" required></textarea>
+                                        </div>
+                                        <p class="text-sm text-gray-500">Are you sure you want to deactivate your account? All of your data will be permanently removed. This action cannot be undone.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                            <button type="button" class="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto">Deactivate</button>
+                            <button type="button" class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto">Cancel</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+
         <div class="container mx-auto px-8 py-8 flex flex-wrap">
             <div class="w-3/12 px-6 py-8 border-gray-200 border-r bg-white shadow-sm rounded-tl-md">
                 <div class="flex items-center gap-x-4">
@@ -36,7 +85,7 @@ defineProps({
                     </div>
                 </div>
                 <div class="mt-6 mb-6 flex flex-col">
-                    <a href="#" class="rounded-md font-light shadow-md bg-purple-500 px-4 py-2 text-center text-white uppercase w-full mb-4">New Project</a>
+                    <a href="#" @click="open = !open" class="rounded-md font-light shadow-md bg-purple-500 px-4 py-2 text-center text-white uppercase w-full mb-4">New Project</a>
                     <a href="#" class="rounded-md font-light shadow-md border border-gray-100 bg-transparent px-4 py-2 text-center text-gray-700 uppercase w-full">New Task</a>
                 </div>
                 <div>
